bebc0697357b4a1ae6fcb638af3ea099e17b43bc
Ryan Parker <ryan.parker3@outlook.com>
2022-02-03 13:17:15 -0800

    issue https://github.com/aws/aws-cdk/issues/18815

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Stack } from "aws-cdk-lib";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
+import {
+  Cluster,
+  FargateTaskDefinition,
+  ContainerImage,
+  FargateService,
+} from "aws-cdk-lib/aws-ecs";
 
 const app = new App();
-const stack = new Stack(app, 'sample');
+const stack = new Stack(app, "sample", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+const vpc = Vpc.fromLookup(stack, "VPC", {
+  isDefault: true,
+});
+
+const cluster = new Cluster(stack, "FargateCPCluster", {
+  vpc,
+  enableFargateCapacityProviders: true,
+});
+
+const taskDefinition = new FargateTaskDefinition(stack, "TaskDef");
+
+taskDefinition.addContainer("web", {
+  image: ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+});
+
+new FargateService(stack, "FargateService", {
+  cluster,
+  taskDefinition,
+});
